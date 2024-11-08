0298412833d6a3e62b87812e37dc087764d74880
Deivid Smarzaro <deivid.smarzaro@bentley.com>
2024-11-08 16:48:23 -0300

    troca print por logging

--- _cicd/avoid_dup_bet.py
+++ _cicd/avoid_dup_bet.py
@@ -1,16 +1,5 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    avoid_dup_bet.py                                   :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/11/06 08:12:15 by umeneses          #+#    #+#              #
-#    Updated: 2024/11/06 08:25:53 by umeneses         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import sys
+import logging
 
 def check_duplicates_bets(file_path):
     with open(file_path, 'r') as file:
@@ -19,11 +8,11 @@ def check_duplicates_bets(file_path):
     seen = {}
     for line_number, line in enumerate(lines, start=1):
         if line in seen:
-            print(f"Duplicated BET found on line {line_number}: '{line}'")
+            logging.info(f"Duplicated BET found on line {line_number}: '{line}'")
             sys.exit(line_number)
         seen[line] = line_number
 
-    print("No duplicates found.")
+    logging.info("No duplicates found.")
     sys.exit(0)
 
 if __name__ == "__main__":
