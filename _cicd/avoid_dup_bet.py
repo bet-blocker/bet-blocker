8fed0ecedf6aa9d195751f2eae5fcb262a414dab
Luiz Gattaz <61541264+LuizDGattaz@users.noreply.github.com>
2024-12-13 19:53:59 -0300

    Update avoid_dup_bet.py
    fiz alguns ajustes nessa parte do código
    Manuseio de Exceções adicionando blocos try-except para lidar com erros, como arquivo não encontrado ou problemas de leitura!
    Deixei os parâmetros dinâmicos assim o nome do arquivo será passado como argumento.
    Deixei mais simples para o usuário o número de duplicatas encontradas.
    Meti uma função para remover duplicatas automaticamente, em vez de apenas sugerir um comando.
    Validação das URL's
    
    Signed-off-by: Luiz Gattaz <61541264+LuizDGattaz@users.noreply.github.com>

--- _cicd/avoid_dup_bet.py
+++ _cicd/avoid_dup_bet.py
@@ -1,28 +1,44 @@
 import os
 import sys
 import logging
+from urllib.parse import urlparse
 
+def is_valid_url(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
 
 def check_duplicates(file_name):
     script_dir = os.path.dirname(__file__)
     parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
     file_path = os.path.join(parent_dir, file_name)
 
-    with open(file_path, 'r') as file:
-        lines = file.read().splitlines()
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.read().splitlines()
+    except FileNotFoundError:
+        logging.error(f"Arquivo {file_name} não encontrado. Verifique se ele existe no diretório esperado.")
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"Erro inesperado ao processar o arquivo: {e}")
+        sys.exit(1)
 
     seen = set()
     duplicates = [f"Linha {i + 1}: '{line}'" for i, line in enumerate(lines) if line in seen or seen.add(line)]
 
     if duplicates:
-        logging.error("Commit rejeitado! URLs duplicadas encontradas:\n" + "\n".join(duplicates))
-        logging.error("Resolva as duplicidades executando: python scripts/remove_duplicates.py")
+        logging.error(f"{len(duplicates)} duplicatas encontradas no arquivo!")
+        logging.error("Detalhes:\n" + "\n".join(duplicates))
         sys.exit(1)
 
     logging.info("Nenhuma duplicidade encontrada.")
     sys.exit(0)
 
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(message)s")
-    check_duplicates('blocklist.txt')
+
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else:
+        file_name = 'blocklist.txt'
+
+    check_duplicates(file_name)
